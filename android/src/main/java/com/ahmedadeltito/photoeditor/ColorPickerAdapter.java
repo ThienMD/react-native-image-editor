ef14483b96c950325e2ea11e4e91535d7001f373
Thien <thienmd@outlook.com>
2023-12-07 15:10:09 +0700

    Update color picker

--- android/src/main/java/com/ahmedadeltito/photoeditor/ColorPickerAdapter.java
+++ android/src/main/java/com/ahmedadeltito/photoeditor/ColorPickerAdapter.java
@@ -3,8 +3,12 @@ package com.ahmedadeltito.photoeditor;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
@@ -16,6 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.BounceInterpolator;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 import ui.photoeditor.R;
@@ -23,6 +28,59 @@ import ui.photoeditor.R;
 /**
  * Created by Ahmed Adel on 5/8/17.
  */
+
+class CheckIconDrawable extends Drawable {
+
+    private final Paint paint;
+    private final int size;
+
+    public CheckIconDrawable(int color, int size) {
+        this.size = size;
+        paint = new Paint();
+        paint.setColor(color);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        int width = getBounds().width();
+        int height = getBounds().height();
+
+        Path path = new Path();
+        path.moveTo(width * 0.3f, height * 0.5f);
+        path.lineTo(width * 0.45f, height * 0.7f);
+        path.lineTo(width * 0.75f, height * 0.4f);
+
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return size;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return size;
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        paint.setAlpha(alpha);
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        paint.setColorFilter(colorFilter);
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
 
 public class ColorPickerAdapter extends RecyclerView.Adapter<ColorPickerAdapter.ViewHolder> {
 
@@ -81,16 +139,27 @@ public class ColorPickerAdapter extends RecyclerView.Adapter<ColorPickerAdapter.
 
     private void buildColorPickerView(View view, int colorCode, boolean isSelected) {
         view.setVisibility(View.VISIBLE);
-        String shapeType = isSelected ? "diamond" : "circle";
-        ShapeDrawable biggerShape = createShapeDrawable(shapeType, 20, colorCode);
-        ShapeDrawable smallerShape = createShapeDrawable(shapeType, 5, Color.WHITE);
-        smallerShape.setPadding(10, 10, 10, 10);
+        ShapeDrawable backgroundShape = createShapeDrawable("circle", 30, Color.WHITE);
+        // smallerShape.setPadding(10, 10, 10, 10);
 
-        Drawable[] drawables = { smallerShape, biggerShape };
+        Drawable[] drawables;
+        ShapeDrawable biggerShape = createShapeDrawable("circle", 20, colorCode);
 
-        LayerDrawable layerDrawable = new LayerDrawable(drawables);
+        if (isSelected) {
+            Drawable checkIcon = new CheckIconDrawable(Color.WHITE, 20); // 20 is the size of the check icon
+            drawables = new Drawable[] { backgroundShape, biggerShape, checkIcon };
 
-        view.setBackgroundDrawable(layerDrawable);
+            LayerDrawable layerDrawable = new LayerDrawable(drawables);
+            int centerX = (biggerShape.getIntrinsicWidth() - checkIcon.getIntrinsicWidth()) / 2;
+            int centerY = (biggerShape.getIntrinsicHeight() - checkIcon.getIntrinsicHeight()) / 2;
+
+            layerDrawable.setLayerInset(2, centerX, centerY, centerX, centerY); // 2 is the index of checkIcon in the drawables array
+            view.setBackgroundDrawable(layerDrawable);
+        } else {
+            view.setBackgroundDrawable(
+                new LayerDrawable(new Drawable[] { backgroundShape, biggerShape })
+            );
+        }
     }
 
     public void setOnColorPickerClickListener(
